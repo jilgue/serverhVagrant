ee251e38cdf03e7307a3cc972f5756bfc5c835b1
César M. Cristóbal <jilgue@gmail.com>
2015-01-04 00:39:21 +0100

    Atributos de apache, añadido mod y sites - fix de mysql

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -1,8 +1,1 @@
-# enable mod rewrite
-execute "/usr/sbin/a2enmod rewrite" do
-  notifies :restart, "service[apache2]"
-  not_if do (::File.symlink?("/etc/apache2/mods-enabled/rewrite.load") and
-             ((::File.exists?("/etc/apache2/mods-available/rewrite.conf"))?
-                (::File.symlink?("/etc/apache2/mods-enabled/rewrite.conf")):(true)))
-  end
-end
+default["apache"]["sitesAvailable"] = ["default"]
--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -1,20 +1,9 @@
-package "mysql-client" do
-  action :install
-end
-
-package "mysql-server" do
-  action :install
-end
-
+package "mysql-client"
+package "mysql-server"
 package "libapache2-mod-auth-mysql"
 package "php5-mysql"
 
 service "mysql" do
   service_name "mysql"
-  /usr/bin/mysql
-  restart_command "/usr/sbin/invoke-rc.d apache2 restart && sleep 1"
-  reload_command "/usr/sbin/invoke-rc.d apache2 reload && sleep 1"
-
-  supports [:restart, :reload, :status]
-  action :enable
+  action [:start, :enable]
 end
