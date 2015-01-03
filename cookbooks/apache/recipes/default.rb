955c339ce4425e56bd9de581bc7b0b31eccc068b
César M. Cristóbal <jilgue@gmail.com>
2015-01-03 21:17:33 +0100

    Enable mod rewrite movido a attributes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -9,12 +9,3 @@ service "apache2" do
   supports [:restart, :reload, :status]
   action :enable
 end
-
-# enable mod rewrite
-execute "/usr/sbin/a2enmod rewrite" do
-  notifies :restart, "service[apache2]"
-  not_if do (::File.symlink?("/etc/apache2/mods-enabled/rewrite.load") and
-             ((::File.exists?("/etc/apache2/mods-available/rewrite.conf"))?
-                (::File.symlink?("/etc/apache2/mods-enabled/rewrite.conf")):(true)))
-  end
-end
