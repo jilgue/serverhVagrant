5ac4bcd9b54d3f2e92a1c1d73cd2e80c30787a01
César M. Cristóbal <jilgue@gmail.com>
2015-01-05 13:31:48 +0100

    Start de apache en el provision

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,7 +4,8 @@ service "apache2" do
   service_name "apache2"
   restart_command "/usr/sbin/invoke-rc.d apache2 restart && sleep 1"
   reload_command "/usr/sbin/invoke-rc.d apache2 reload && sleep 1"
-  action :enable
+  supports [:restart, :reload, :status, :start]
+  action [:start, :enable]
 end
 
 # enable mod rewrite
